1af58646d0990376f9744fdf8c5cf8f2dbff8b22
Mahir-Asef <mahirasef30@gmail.com>
2022-08-18 18:46:18 +0600

    done

---
+++ banking.js
@@ -0,0 +1,61 @@
+document.getElementById("add-here").addEventListener("click", function() {
+
+    //inputField
+    const depositAmount =  document.getElementById("deposit");
+    const totalDepositText = depositAmount.value;
+     //    console.log(depositAmount);
+    const totalDepositAmount = parseFloat(totalDepositText);
+   
+
+    //taggg
+    const allDeposit = document.getElementById("total-added");
+    allDepositText=allDeposit.innerText;
+    const totalDeposit = parseFloat(allDepositText);
+    // console.log(allDeposit);
+
+    // Total Money Section
+    const money = document.getElementById("total-money");
+    allMoneyText=money.innerText;
+    const totalMoney = parseFloat(allMoneyText);
+    // console.log(totalMoney);
+
+    // Total deposit here
+    if(totalDepositAmount>0){
+        allDeposit.innerText =totalDeposit+totalDepositAmount;
+        money.innerText = totalMoney+totalDepositAmount;
+    }
+    // console.log(totalMoney)
+    depositAmount.value='';
+  });
+
+
+document.getElementById("withdraw-here").addEventListener("click", function() {
+
+    //inputField
+    const withdrawAmount =  document.getElementById("withdraw");
+    const totalWithdrawText = withdrawAmount.value;
+     //    console.log(depositAmount);
+    const totalWithdrawAmount = parseFloat(totalWithdrawText);
+   
+
+    //textField
+    const allWithdraw = document.getElementById("total-withdraw");
+    allWithdrawText=allWithdraw.innerText;
+    const totalWithdraw = parseFloat(allWithdrawText);
+    // console.log(allDeposit);
+
+    // Total Money Section
+    const money = document.getElementById("total-money");
+    allMoneyText=money.innerText;
+    const totalMoney = parseFloat(allMoneyText);
+    console.log(totalMoney);
+
+    // Total Withdraw here
+    
+    if(totalWithdrawAmount>0 && totalMoney>=totalWithdrawAmount){
+        allWithdraw.innerText =totalWithdraw+totalWithdrawAmount;
+        money.innerText = totalMoney-totalWithdrawAmount;
+    }
+    // console.log(totalMoney)
+    withdrawAmount.value='';
+  });
